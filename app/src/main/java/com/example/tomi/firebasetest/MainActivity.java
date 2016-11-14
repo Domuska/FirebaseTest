a34500f6c883903c8aafd331953941a96a668dbc
Tomi Lamsa <lamsatom@gmail.com>
2016-11-14 14:55:16 +0200

    added way to add on item click listener to the recyclerview items

--- app/src/main/java/com/example/tomi/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/tomi/firebasetest/MainActivity.java
@@ -5,13 +5,16 @@ import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -23,9 +26,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
-    private final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity";
     private TextView textView;
     private EditText editText;
 
@@ -120,7 +125,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
 
         adapter = new FirebaseRecyclerAdapter<JournalEntry, JournalEntryHolder>(
                         JournalEntry.class,
@@ -128,14 +134,15 @@ public class MainActivity extends AppCompatActivity {
                         JournalEntryHolder.class,
                         journalReference) {
 
+
             @Override
             protected void populateViewHolder(JournalEntryHolder viewHolder, JournalEntry entry, int position) {
                 //here we set the text to the fields
                 viewHolder.setTitle(entry.getTitle());
                 viewHolder.setBody(entry.getBodyText());
+                viewHolder.setClickListener(entry);
             }
         };
-
 
         recyclerView.setAdapter(adapter);
 
@@ -179,6 +186,26 @@ public class MainActivity extends AppCompatActivity {
 
         public void setBody(String body){
             ((TextView)view.findViewById(android.R.id.text2)).setText(body);
+        }
+
+        public void setClickListener(final JournalEntry entry){
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, "item was clicked:" + entry.getTitle());
+                }
+            });
+
+            //jos halutaan, voidaan etsiä yksittäinen elementti koko kortista johon lisätään
+            //teksti, kuten esmes alla:
+
+            /*final TextView t = (TextView) view.findViewById(android.R.id.text2);
+            t.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, "item was clicked:" + t.getText());
+                }
+            });*/
         }
     }
 }
